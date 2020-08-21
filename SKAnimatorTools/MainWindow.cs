03cf5df38573d212a7cbcc2c78c856a4a8112d26
Xan <subzeroxan@gmail.com>
2020-08-21 02:31:37 -0500

    version bump

--- SKAnimatorTools/MainWindow.cs
+++ SKAnimatorTools/MainWindow.cs
@@ -89,7 +89,7 @@ namespace SKAnimatorTools {
 		/// <summary>
 		/// The version of this release of the program.
 		/// </summary>
-		public readonly int[] THIS_VERSION = { 2, 1, 0 };
+		public readonly int[] THIS_VERSION = { 2, 1, 1 };
 
 		/// <summary>
 		/// Attempts to access the github to acquire the latest version.
