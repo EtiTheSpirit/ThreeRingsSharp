dc84d01904fea6563e1b8b38004f4459979104c3
EtiTheSpirit <subzeroxan@gmail.com>
2021-04-12 11:04:42 -0500

    Update version.

--- SKAnimatorTools/MainWindow.cs
+++ SKAnimatorTools/MainWindow.cs
@@ -87,7 +87,7 @@ namespace SKAnimatorTools {
 		/// <summary>
 		/// The version of this release of the program.
 		/// </summary>
-		public readonly int[] THIS_VERSION = { 2, 3, 1 };
+		public readonly int[] THIS_VERSION = { 2, 3, 2 };
 
 		/// <summary>
 		/// Attempts to access the github to acquire the latest version.
