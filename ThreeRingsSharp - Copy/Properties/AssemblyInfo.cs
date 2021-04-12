d9e3f4cb32168f732c8c4b1fdfe04a9dfc677bc4
EtiTheSpirit <subzeroxan@gmail.com>
2021-04-12 10:37:44 -0500

    Remove duplicate of project

--- ThreeRingsSharp - Copy/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Converts models from Grey Havens's \"Clyde\" library into the glTF 2.0 format.")]
-[assembly: AssemblyDescription("Converts models from Grey Havens's \"Clyde\" library into the glTF 2.0 format.")]
-[assembly: AssemblyCompany("Xan the Dragon // Eti the Spirit")]
-[assembly: AssemblyProduct("ThreeRingsSharp")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
